e1b6e74ad32f0d5242aa6c0d4e630108d1344b6e
Leesa Ward <leesa@doubleedesign.com.au>
2026-04-12 12:05:38 +1000

    Feat(ResizeHandler): Separate and test logic for calculating available space including siblings

---
+++ packages/resize-handle/src/ContextHandler.ts
@@ -0,0 +1,62 @@
+import { ResizeHandlePosition } from "./types";
+
+export class ContextHandler {
+	private element: HTMLElement;
+	private container: HTMLElement = document.body;
+
+	constructor({ element, container }: { element: HTMLElement, container?: HTMLElement }) {
+		this.element = element;
+		this.container = (container instanceof HTMLElement) ? container : (element.parentElement ?? document.body);
+	}
+
+	getSiblingsBefore(): Element[] {
+		const nodes = Array.from(this.container?.children ?? []);
+
+		const elementIndex = nodes.indexOf(this.element);
+		if (elementIndex === -1) return [];
+
+		return Array.from(nodes).slice(0, elementIndex);
+	}
+
+	getSiblingsAfter(): Element[] {
+		const nodes = Array.from(this.container?.children ?? []);
+
+		const elementIndex = nodes.indexOf(this.element);
+		if (elementIndex === -1) return [];
+
+		return Array.from(nodes).slice(elementIndex + 1);
+	}
+
+	calculateFreeSpace(side: ResizeHandlePosition): number {
+		const containerRect = this.container.getBoundingClientRect();
+		const elementRect = this.element.getBoundingClientRect();
+
+		const calculatingVertical = side === ResizeHandlePosition.TOP || side === ResizeHandlePosition.BOTTOM;
+
+		const siblings = (side === ResizeHandlePosition.LEFT || side === ResizeHandlePosition.TOP)
+			? this.getSiblingsBefore()
+			: this.getSiblingsAfter();
+
+		const siblingsSize = siblings.reduce((total, sibling) => {
+			const siblingRect = sibling.getBoundingClientRect();
+
+			return total + (calculatingVertical ? siblingRect.height : siblingRect.width);
+		}, 0);
+
+		const elementSize = calculatingVertical ? elementRect.height : elementRect.width;
+		const containerSize = calculatingVertical ? containerRect.height : containerRect.width;
+
+		// If we want the space on the left or top, we just need to compare the element's position to the container edge
+		// and subtract the size of any siblings in the way
+		if (side === ResizeHandlePosition.LEFT || side === ResizeHandlePosition.TOP) {
+			return (elementRect[calculatingVertical ? 'top' : 'left'] - containerRect[calculatingVertical ? 'top' : 'left']) - siblingsSize;
+		}
+
+		// If we want the space on the right or bottom, we need to also subtract the distance from the element to the container edge
+		const distanceToContainerEdge = calculatingVertical
+			? containerRect.bottom - elementRect.bottom
+			: containerRect.right - elementRect.right;
+
+		return distanceToContainerEdge - siblingsSize;
+	}
+}
---
+++ packages/resize-handle/tests/context-handler.test.ts
@@ -0,0 +1,129 @@
+import { ContextHandler } from "../src/ContextHandler";
+import { ResizeHandlePosition } from "../src/types";
+import { mockBoundingClientRect } from "./utils";
+
+function getOppositeSide(side: ResizeHandlePosition): ResizeHandlePosition {
+	switch (side) {
+		case ResizeHandlePosition.RIGHT:
+			return ResizeHandlePosition.LEFT;
+		case ResizeHandlePosition.LEFT:
+			return ResizeHandlePosition.RIGHT;
+		case ResizeHandlePosition.TOP:
+			return ResizeHandlePosition.BOTTOM;
+		case ResizeHandlePosition.BOTTOM:
+			return ResizeHandlePosition.TOP;
+	}
+}
+
+describe('ContextHandler', () => {
+	let container: HTMLElement;
+	let elements = [] as HTMLElement[];
+
+	beforeEach(() => {
+		container = document.createElement('div');
+		container.id = 'container';
+		container.style.width = '1000px';
+
+		elements = Array.from({ length: 4 }, (_, i) => {
+			const element = document.createElement('div');
+			element.id = `element${i + 1}`;
+			element.style.width = '200px';
+			element.style.height = '100px';
+
+			return element;
+		});
+
+		container.append(...elements);
+	});
+
+	it('returns a single sibling before the element', () => {
+		const instance = new ContextHandler({ element: elements[1], container });
+
+		expect(instance.getSiblingsBefore()).toEqual([elements[0]]);
+	});
+
+	it('returns multiple siblings before the element', () => {
+		const instance = new ContextHandler({ element: elements[2], container });
+
+		expect(instance.getSiblingsBefore()).toEqual([elements[0], elements[1]]);
+	});
+
+	it('returns a single sibling after the element', () => {
+		const instance = new ContextHandler({ element: elements[2], container });
+
+		expect(instance.getSiblingsAfter()).toEqual([elements[3]]);
+	});
+
+	it('returns multiple siblings after the element', () => {
+		const instance = new ContextHandler({ element: elements[1], container });
+
+		expect(instance.getSiblingsAfter()).toEqual([elements[2], elements[3]]);
+	});
+
+	describe.each([
+		ResizeHandlePosition.RIGHT,
+		ResizeHandlePosition.LEFT,
+		ResizeHandlePosition.TOP,
+		ResizeHandlePosition.BOTTOM,
+	])('calculates free space on the %s correctly', (side) => {
+		let container: HTMLElement;
+		let element: HTMLElement;
+		let sibling1: HTMLElement;
+		let sibling2: HTMLElement;
+
+		container = document.createElement('div');
+		container.id = 'container';
+		container.style.width = '1000px';
+
+		element = document.createElement('div');
+		element.id = 'element';
+
+		sibling1 = document.createElement('div');
+		sibling1.id = 'sibling1';
+
+		sibling2 = document.createElement('div');
+		sibling2.id = 'sibling2';
+
+		beforeEach(() => {
+			mockBoundingClientRect(container, 1000, 1000);
+		});
+
+		test('when there are no siblings', () => {
+			mockBoundingClientRect(element, 200, 200);
+			container.append(element);
+
+			const instance = new ContextHandler({ element, container });
+
+			expect(instance.calculateFreeSpace(side as ResizeHandlePosition)).toEqual(800);
+		});
+
+		test('when there are siblings on the handle side', () => {
+			mockBoundingClientRect(element, 200, 200, getOppositeSide(side));
+			mockBoundingClientRect(sibling1, 300, 300, undefined, 200);
+			mockBoundingClientRect(sibling2, 300, 300, undefined, 500);
+
+			if (side === ResizeHandlePosition.RIGHT || side === ResizeHandlePosition.BOTTOM) {
+				container.append(element, sibling1, sibling2);
+			}
+			else {
+				container.append(sibling1, sibling2, element);
+			}
+
+			const instance = new ContextHandler({ element, container });
+
+			expect(instance.calculateFreeSpace(side as ResizeHandlePosition)).toEqual(200);
+		});
+
+		test('when there are siblings on both sides', () => {
+			mockBoundingClientRect(element, 200, 200, undefined, 300);
+			mockBoundingClientRect(sibling1, 300, 300, undefined, 0);
+			mockBoundingClientRect(sibling2, 300, 300, undefined, 500);
+
+			container.append(sibling1, element, sibling2);
+
+			const instance = new ContextHandler({ element, container });
+
+			expect(instance.calculateFreeSpace(side as ResizeHandlePosition)).toEqual(500);
+		});
+	});
+});
