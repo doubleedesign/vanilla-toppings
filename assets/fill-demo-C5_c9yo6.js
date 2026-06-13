5ccdd445d0dbf25a9ef7f421648091fb11f5413a
doubleedesign <doubleedesign@users.noreply.github.com>
2026-06-13 07:22:30 +0000

    deploy: cdac6f0013a068d67a51268d0d00bedffdc4cc9a

---
+++ assets/fill-demo-C5_c9yo6.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-BneVvdWh.js";var t;e((()=>{t=`@use '../../hover' as *;
+
+.fancy-link-fill {
+	font-family: "Arial", sans-serif;
+	padding: 0.5rem;
+	border: 1px solid #EDEDED;
+	color: black;
+	@include fill-from-left();
+}`}))();export{t as default};
